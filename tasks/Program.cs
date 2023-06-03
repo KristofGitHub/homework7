ac11fc87a4fbb44c40c7d00938b6a7470bb5193f
Vasiliev Sergei <sergei.binp@gmail.com>
2023-06-03 23:05:32 +0500

    2nd commit: second task

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -4,12 +4,12 @@ m = 3, n = 4.
 0,5 7 -2 -0,2
 1 -3,3 8 -9,9
 8 7,8 -7,1 9
-*/
+
 Console.WriteLine("TASK # 1");
 Console.WriteLine("");
 Console.WriteLine("Input number of rows: ");
 int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number of columnss: ");
+Console.WriteLine("Input number of columns: ");
 int columns = Convert.ToInt32(Console.ReadLine());
 
 double[,] myArray = Create2DMatrix(rows, columns);
@@ -40,15 +40,72 @@ void Print2DArray(double[,] array)
         Console.WriteLine();
     }
 }
+*/
 
 /*
-Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента 
+или же указание, что такого элемента нет.
 Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
 17 -> такого числа в массиве нет
+*/
+Console.WriteLine("TASK # 2");
+Console.WriteLine("");
+Console.WriteLine("Input number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
 
+int[,] myArray = Create2DMatrix(rows, columns);
+Print2DArray(myArray);
+
+Console.WriteLine("Input number of rows finding elements of array: ");
+int rowFE = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns finding elements of array: ");
+int columnFE = Convert.ToInt32(Console.ReadLine());
+
+FindElement(myArray, rowFE, columnFE);
+
+int[,] Create2DMatrix (int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    Console.WriteLine("Yours array: ");
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElement(int[,] array, int rowFE, int columnFE)
+{
+    Console.WriteLine("");
+    if ((array.GetLength(0) + 1) >= rowFE)
+    {
+        if ((array.GetLength(1) + 1) >= columnFE) Console.WriteLine($"Your elements is {array[rowFE - 1, columnFE - 1]}");
+        else Console.WriteLine("This element does not exist in your array");
+    }
+    else Console.WriteLine("This element does not exist in your array");
+}
+
+/*
 Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 Например, задан массив:
 1 4 7 2
