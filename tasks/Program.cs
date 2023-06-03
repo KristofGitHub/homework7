d10bb14e69420b3394056b80e77feafec6011a62
Vasiliev Sergei <sergei.binp@gmail.com>
2023-06-03 22:47:17 +0500

    1st commit: first task

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -1,4 +1,71 @@
-﻿Console.WriteLine("            *   *   *");
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+Console.WriteLine("TASK # 1");
+Console.WriteLine("");
+Console.WriteLine("Input number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columnss: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = Create2DMatrix(rows, columns);
+Print2DArray(myArray);
+
+double[,] Create2DMatrix (int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble() * 2 - 1, 1);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(double[,] array)
+{
+    Console.WriteLine("Yours array: ");
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+
+
+
+
+
+
+/*
+
+Console.WriteLine("            *   *   *");
 Console.WriteLine("CREATING AND PRINTING 2D-ARRAYS CODE");
 Console.WriteLine("");
 Console.WriteLine("Input number of rows: ");
@@ -88,3 +155,4 @@ int[,] ModifyMy2DMatrix (int[,] array)
     }
     return array;
 }
+ */
